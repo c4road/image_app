cbafbd13c2d7daaf11b68428891016b30cf5d517
Alberto <rinconegocio@gmail.com>
2020-11-01 19:31:08 -0300

    adding search endpoint

--- endpoints/images/controllers.py
+++ endpoints/images/controllers.py
@@ -1,16 +1,50 @@
 from flask_restful import Resource
+from cache import cache
+from utils import get_all_images
 
+
+def get_images_from_cache():
+    images = cache.get('images')
+    if not images:
+        cache.set('images', get_all_images())
+    
+    return images
+
+
+
+class ImageSearchController(Resource):
+    """This controller is responsible for accounts operations"""
+    def get(self, search_term):
+        """Create an account within the XCurrent platform"""
+        images = get_images_from_cache()
+        search_term = str(search_term).lower().strip()
+        response = {
+            "image": "No results found"
+        }
+        search_results = []
+        for image in images:
+            print(image)
+            id_ = image.get('id', '')
+            author = image.get('author', '').lower()
+            camera = image.get('camera', '').lower()
+            tags = image.get("tags", '').lower()
+
+            if (search_term == id_) or \
+                (search_term == author) or \
+                (search_term in camera) or \
+                (search_term in tags):
+                search_results.append(image)
+                break
+        response.update({"results": search_results})
+        return response
 
 
 class ImageController(Resource):
-    """This controller is responsible for accounts operations"""
 
     def get(self):
         """Create an account within the XCurrent platform"""
-        endpoint = "/config/accounts"
-
-        return {"hola": "mundo"}
-    
-
-class ImageSearchController(Resource):
-    pass
+        images = get_images_from_cache()
+        response = {
+            'images': images
+        }
+        return response
